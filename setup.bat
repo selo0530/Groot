9958b58dc55a537f0b2a604b597746e960ea00eb
jeongrae.kim <selo0530@gmail.com>
2022-02-24 17:12:56 +0900

    add setup.bat

---
+++ setup.bat
@@ -0,0 +1,9 @@
+mkdir build
+cd build
+
+cmake ^
+	-G "Visual Studio 16 2019" ^
+	-DCMAKE_MODULE_PATH="E:/github/qt-everywhere-src-5.15.2/qtbase" ^
+	-DQt5_DIR="E:/github/qt-everywhere-src-5.15.2/qtbase/lib/cmake/Qt5" ^
+	-DBUILD_SAMPLES=OFF ^
+    ../
