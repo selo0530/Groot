5d21a3d8dd7d9a0ff53f92893ce9dbea3575a396
jeongrae.kim <selo0530@gmail.com>
2022-02-24 17:32:47 +0900

    using QTBASE_DIR env var

--- setup.bat
+++ setup.bat
@@ -3,8 +3,8 @@ cd build
 
 cmake ^
 	-G "Visual Studio 16 2019" ^
-	-DCMAKE_MODULE_PATH="E:/github/qt-everywhere-src-5.15.2/qtbase" ^
-	-DQt5_DIR="E:/github/qt-everywhere-src-5.15.2/qtbase/lib/cmake/Qt5" ^
+	-DCMAKE_MODULE_PATH="%QTBASE_DIR%" ^
+	-DQt5_DIR="%QTBASE_DIR%/lib/cmake/Qt5" ^
 	-DBUILD_SAMPLES=OFF ^
 	-DBUILD_SHARED_LIBS=OFF ^
     ../
