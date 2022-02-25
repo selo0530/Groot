cdd1e6af8fe611ed5f427399d807fea25e126cd8
jeongrae.kim <selo0530@gmail.com>
2022-02-25 17:20:59 +0900

    빌드 세팅법 추가

--- setup.bat
+++ setup.bat
@@ -2,7 +2,7 @@ mkdir build
 cd build
 
 cmake ^
-	-G "Visual Studio 16 2019" ^
+	-G "Visual Studio 17 2022" ^
 	-DCMAKE_MODULE_PATH="%QTBASE_DIR%" ^
 	-DQt5_DIR="%QTBASE_DIR%/lib/cmake/Qt5" ^
 	-DBUILD_SAMPLES=OFF ^
